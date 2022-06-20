1716561ecb84a9a4baf6744e23e8ba7b13edcd02
lloydlobo <lloyd@lloydlobo.com>
2022-06-20 13:26:47 +0530

    feat: write first pseudo code draft

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,28 @@
 // cspell:ignore btns
 // https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code#handle_keyboard_events_in_a_game
-const hashMap = new Map();
+// const hashMap = new Map();
+
+let bill = 0;
+let userTipChoice = 0;
+let userTipCustomChoice = 0;
+let userTotalPeopleInput = 0;
 
 function getInputBillAmount(bill) {
   const userInputTotalBill = bill;
+  return parseInt(userInputTotalBill, 10);
 }
 function getInputTipPercent(userTipChoice) {
   const userInputTipPercent = userTipChoice;
+  return parseInt(userInputTipPercent, 10);
 }
 function getInputCustomTip(userTipCustomChoice) {
   const userInputCustomTip = userTipCustomChoice;
+  return parseInt(userInputCustomTip, 10);
 }
 function getInputTotalPeople(userTotalPeopleInput) {
   const userInputTotalPeople = userTotalPeopleInput;
+  return parseInt(userInputTotalPeople, 10);
 }
-// calculateTip();
-// reset();
 
 const inputBill = document.querySelector("#inputBill");
 inputBill.addEventListener("keydown", (event) => {
@@ -57,17 +64,59 @@ inputStepTotalPeople.addEventListener("keydown", (event) => {
   } // end switch
 });
 
-function runCalculateTip() {
-  getInputBillAmount();
-  getInputTipPercent();
-  // getInputCustomTip();
-  // getInputTotalPeople();
-  // calculateTip();
-  // reset();
+/// //////////////////////////////////////////////////////////////////////////////
+/// region:      --- Logic ---
+/// //////////////////////////////////////////////////////////////////////////////
+const calculateTipAmount = (bill, tip) => (bill * tip) / 100;
+
+const getTipAmountChoices = (bill, calculatedTip, tip, tipCustom) => {
+  if (tipCustom !== 0 && tipCustom !== "" && tipCustom !== null) {
+    calculatedTip = calculateTipAmount(bill, tipCustom);
+  } else {
+    calculatedTip = calculateTipAmount(bill, tip);
+  }
+  return calculatedTip;
+};
+
+const calculateTotalPerPerson = (bill, calculatedTip, totalPeople) =>
+  (bill + calculatedTip) / totalPeople;
+
+async function runCalculateTip() {
+  const resultBill = getInputBillAmount(bill);
+  const resultTip = getInputTipPercent(userTipChoice);
+  const resultTipCustom = getInputCustomTip(userTipCustomChoice);
+  const resultTotalPeople = getInputTotalPeople(userTotalPeopleInput);
+
+  let tipAmount = 0;
+
+  tipAmount = getTipAmountChoices(
+    resultBill,
+    tipAmount,
+    resultTip,
+    resultTipCustom
+  );
+
+  const totalPerPerson = calculateTotalPerPerson(
+    resultBill,
+    tipAmount,
+    resultTotalPeople
+  );
+
+  return { tipAmount, totalPerPerson };
 }
 
-function main() {
-  runCalculateTip();
+/// //////////////////////////////////////////////////////////////////////////////
+/// region:      --- Reset ---
+/// //////////////////////////////////////////////////////////////////////////////
+
+/// //////////////////////////////////////////////////////////////////////////////
+/// region:      --- Main ---
+/// //////////////////////////////////////////////////////////////////////////////
+
+async function main() {
+  const results = await runCalculateTip();
+  const { tipAmount, totalPerPerson } = results;
+  console.log(tipAmount, totalPerPerson);
 }
 
 main();
