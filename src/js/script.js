cd687e56b23c637f8a94daae41ae42596371c1ee
lloydlobo <lloyd@lloydlobo.com>
2022-06-20 15:31:34 +0530

    fix: promise resolve splitCalculate

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,8 @@ const tipPerPerson = document.querySelector("#tipPerPerson");
 const totalPerPerson = document.querySelector("#totalPerPerson");
 const btnReset = document.querySelector("#btnReset");
 
+const btnCalculateSplitter = document.querySelector("#btnCalculateSplitter");
+
 let bill = 0;
 let tip = 0;
 let people = 1;
@@ -26,9 +28,11 @@ async function eventUserBill() {
           "file: script.js | line 22 | inputBill.addEventListener | bill",
           bill
         );
-        break;
+        return bill;
     }
+    return bill;
   });
+  return bill;
 }
 
 async function eventUserCustomTip() {
@@ -38,9 +42,11 @@ async function eventUserCustomTip() {
       case "Tab":
         console.log(inputCustomTip.value);
         tip = (await inputCustomTip.value) * 0.01;
-        break;
+        return tip;
     }
+    return tip;
   });
+  return tip;
 }
 
 async function eventUserTip() {
@@ -52,8 +58,11 @@ async function eventUserTip() {
         "file: script.js | line 30 | btnTip.addEventListener | tip",
         tip
       );
+      return tip;
     });
+    return tip;
   });
+  return tip;
 }
 
 async function eventUserTotalPeople() {
@@ -63,9 +72,11 @@ async function eventUserTotalPeople() {
       case "Tab":
         console.log(inputStepTotalPeople.value);
         people = await inputStepTotalPeople.value;
-        break;
+        return people;
     }
+    return people;
   });
+  return people;
 }
 
 btnReset.addEventListener("click", () => {
@@ -78,3 +89,54 @@ btnReset.addEventListener("click", () => {
   tip = 0;
   people = 1;
 });
+
+async function handleUserInputs() {
+  const resBill = await eventUserBill();
+  const resTip = eventUserCustomTip();
+  // const resTipCustom =  eventUserTip();
+  const resPeople = eventUserTotalPeople();
+  console.log(resBill, resTip, resPeople);
+  return new Promise((resolve, reject) => {
+    if (resBill && resTip && resPeople) {
+      resolve(splitTipLogic(resBill, resTip, resPeople));
+    } else {
+      reject("error");
+    }
+  });
+  return { resBill, resTip, resPeople };
+}
+
+btnCalculateSplitter.addEventListener("click", () => {
+  handleUserInputs().then((res) => {
+    bill = res.resBill;
+    tip = res.resTip;
+    people = res.resPeople;
+
+    splitTipLogic(bill, tip, people);
+  });
+});
+
+function splitTipLogic(bill, tip, people) {
+  let tipPerPerson = (bill * tip) / people;
+  let totalPerPerson = bill + tipPerPerson;
+  tipPerPerson = tipPerPerson.toString();
+  totalPerPerson = totalPerPerson.toString();
+
+  tipPerPerson = tipPerPerson.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  totalPerPerson = totalPerPerson.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+  console.log(tipPerPerson, totalPerPerson);
+}
+
+function init() {
+  eventUserBill();
+  eventUserCustomTip();
+  eventUserTip();
+  eventUserTotalPeople();
+}
+
+function main() {
+  init();
+}
+
+main();
